6f0b5eb9cee610e62dab579d999cdbe38a7d9371
mas24024 <mas24024@byui.edu>
2025-10-01 06:26:27 -0600

    Changed semantics structure for Mission/Index. Also changed css and js files to match new structure.

--- mission/mission.js
+++ mission/mission.js
@@ -1,14 +1,16 @@
 const themeSelector = document.querySelector('#theme');
-const body = document.querySelector('body');
+const body = document.body;
 const logo = document.querySelector('#logo');
 
 function changeTheme() {
   if (themeSelector.value === 'dark') {
     body.classList.add('dark');
-    logo.setAttribute('src', '/mission/images/byui-logo-white.png');
+    logo.src = 'images/byui-logo-white.png';   // dark mode logo
+    logo.alt = 'BYU-Idaho White Logo';
   } else {
     body.classList.remove('dark');
-    logo.setAttribute('src', '/mission/images/byui-logo-blue.webp');
+    logo.src = 'images/byui-logo-blue.webp';   // light mode logo
+    logo.alt = 'BYU-Idaho Blue Logo';
   }
 }
 
